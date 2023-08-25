e30f4df85c2796da9f62c8d5422464ab9faf58fd
Muhammad Abrar H <108718748+MuhammadAbrarHma@users.noreply.github.com>
2023-08-25 16:13:58 +0530

    Add files via upload

---
+++ JPAQueryDay7/src/main/java/com/example/day7/JpaQueryDay7Application.java
@@ -0,0 +1,13 @@
+package com.example.day7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryDay7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueryDay7Application.class, args);
+	}
+
+}
